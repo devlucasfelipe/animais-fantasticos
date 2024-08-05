1831c92f417de8a78b74a14cd0b611f65a3fd72c
Lucas Felipe <lukaofelps@gmail.com>
2024-08-05 20:28:28 -0300

    Modal refatorado com class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+/* eslint-disable quotes */
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -20,11 +21,19 @@ accordion.init();
 
 const tabNav = new TabNav(
   '[data-tab="menu"] li',
+  // eslint-disable-next-line comma-dangle
   '[data-tab="content"] section'
 );
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  // eslint-disable-next-line comma-dangle
+  '[data-modal="container"]'
+);
+modal.init();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
