fea06a84bc6603044c3d6e49a579583e56d55b8b
Lucas Felipe <lukaofelps@gmail.com>
2024-08-06 21:13:45 -0300

    refatoracao com class do scroll anima

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import ScrollAnima from "./modules/scroll-anima.js";
 
 // eslint-disable-next-line quotes
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -37,10 +37,12 @@ modal.init();
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initAnimacaoScroll();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
