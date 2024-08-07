d65ab8c5cef8b2d378e4cd66ad5b5b2121337782
Lucas Felipe <lukaofelps@gmail.com>
2024-08-07 20:59:23 -0300

    Acertado o caminho da API

--- js/script.js
+++ js/script.js
@@ -49,6 +49,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
